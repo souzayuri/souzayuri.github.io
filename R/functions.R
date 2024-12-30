700f1b1c4b155dc283085fcafdf9fc546fa61ca7
unknown <desouza.s.yuri@gmail.com>
2024-12-30 02:44:24 -0500

    picturs2

--- R/functions.R
+++ R/functions.R
@@ -15,14 +15,17 @@ if (!require("tidytuesdayR")) pak::pak("dslc-io/tidytuesdayR")
 resize_image <- function(image) {
   
   imFile <- image_read(here::here(paste0("gallery/img/", image)))
-  imFile_resized <- magick::image_resize(imFile, "6%")
+  imFile_resized <- magick::image_resize(imFile, "20%")
   magick::image_write(imFile_resized, here::here(paste0("gallery/img/thumb-", image)))
   
 }
 
+list_png <- list.files("gallery/img")
+lapply(list_png, resize_image)
+
 make_gallery_layout <- function() {
   
-  images <- list.files("gallery/img")
+  images <- list.files("gallery/img/")
   images_full_size <- grep("thumb", images, 
                            value = TRUE, invert = TRUE)
   images_thumb <- grep("thumb", images, value = TRUE)
